2a711126f54bf2c3aa3cfcc3d6249e1296f2f8ee
Darrell Theng <theng.darrell@gmail.com>
2018-02-03 22:26:34 -0800

    Add files via upload

---
+++ Lab1/CECS475.PokerHand/CECS475.Poker.Model/PokerHandClassifier.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CECS475.Poker.Cards
+{
+    public class PokerHandClassifier
+    {
+        public static PokerHand ClassifyHand(IEnumerable<Card> original)
+        {
+            List<Card> copy = (List<Card>) original;
+            copy.Sort();
+            int[] ofKind = new int[] { 1, 1 };
+            int j = 0; //Used to go to next index in ofKind when some(2-4) kind is found
+            bool isStraight = true;
+            bool isFlush = true;
+        
+            for(int i = 0; i < 4; i++)
+            {
+                //Check for pairs, 3 & 4 of kinds
+                if(copy[i].CompareTo(copy[i+1]) == 0)
+                {
+                    ofKind[j] = ofKind[j] + 1;
+                }
+                else if(ofKind[0] != 1)
+                {
+                    j = 1;
+                }
+                
+                //Check for straight
+                if(copy[i].Kind != copy[i+1].Kind - 1)
+                {
+                    isStraight = false;
+                }
+                //Check for flush
+                if(copy[i].Suit.CompareTo(copy[i+1].Suit) != 0)
+                {
+                    isFlush = false;
+                }
+            }
+
+            //Check Straight Flush
+            if (isStraight && isFlush)
+            {
+                if((int) copy[0].Kind == 10)
+                {
+                    //Royal Flush
+                    return new PokerHand((List<Card>)original, (PokerHand.HandType) 9);
+                }
+                else
+                {
+                    //Straight Flush
+                    return new PokerHand((List<Card>)original, (PokerHand.HandType) 8);
+                }
+            }
+            //Check Straight
+            else if (isStraight)
+            {
+                return new PokerHand((List<Card>)original, (PokerHand.HandType) 4);
+            }
+            //Check Flush
+            else if (isFlush)
+            {
+                return new PokerHand((List<Card>)original, (PokerHand.HandType) 5);
+            }
+            //Check 4 of Kind
+            else if (ofKind[0] == 4)
+            {
+                return new PokerHand((List<Card>)original, (PokerHand.HandType) 7);
+            }
+            //Check Full House
+            else if ((ofKind[0] == 2 && ofKind[1] == 3) || (ofKind[0] == 3 && ofKind[1] == 2))
+            {
+                return new PokerHand((List<Card>)original, (PokerHand.HandType)6);
+            }
+            //Check 3 of Kind
+            else if(ofKind[0] == 3)
+            {
+                return new PokerHand((List<Card>)original, (PokerHand.HandType) 3);
+            }
+            //Check Pair
+            else if(ofKind[0] == 2)
+            {
+                //Check if there is another Pair
+                if (ofKind[1] == 2)
+                {
+                    //2 Pair
+                    return new PokerHand((List<Card>) original, (PokerHand.HandType) 2);
+                }
+                else
+                {
+                    //Pair
+                    return new PokerHand((List<Card>)original, (PokerHand.HandType) 1);
+                }
+            }
+            else
+            {
+                //High Card
+                return new PokerHand((List<Card>)original, (PokerHand.HandType) 0);
+            }
+        }
+    }
+}
---
+++ Lab1/CECS475.PokerHand/PrintTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CECS475.Poker.Cards;
+namespace PrintTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PokerHand h1 = PokerHandClassifier.ClassifyHand(new List<Card> {
+                new Card(Card.CardKind.Queen, Card.CardSuit.Clubs),
+                new Card(Card.CardKind.Jack, Card.CardSuit.Clubs),
+                new Card(Card.CardKind.Ten, Card.CardSuit.Hearts),
+                new Card(Card.CardKind.Two, Card.CardSuit.Diamonds),
+                new Card(Card.CardKind.Five, Card.CardSuit.Spades)});
+
+            PokerHand h2 = PokerHandClassifier.ClassifyHand(new List<Card> {
+                new Card(Card.CardKind.Queen, Card.CardSuit.Diamonds),
+                new Card(Card.CardKind.Jack, Card.CardSuit.Diamonds),
+                new Card(Card.CardKind.Three, Card.CardSuit.Hearts),
+                new Card(Card.CardKind.Two, Card.CardSuit.Diamonds),
+                new Card(Card.CardKind.Five, Card.CardSuit.Spades)});
+
+            Console.WriteLine(h1.CompareTo(h2));
+
+        }
+    }
+}
