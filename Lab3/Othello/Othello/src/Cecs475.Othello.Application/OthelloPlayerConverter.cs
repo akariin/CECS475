67c684a53bdac203f1a2ede57909ec7ce894072f
Darrell Theng <theng.darrell@gmail.com>
2018-03-18 14:10:00 -0700

    Added Othello Lab

---
+++ Lab3/Othello/Othello/src/Cecs475.Othello.Application/OthelloPlayerConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Cecs475.Othello.Application
+{
+    public class OthelloPlayerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int player = (int)value;
+            String currPlayer = "";
+            if(player == 2)
+            {
+                currPlayer = "White";
+            }
+            else
+            {
+                currPlayer = "Black";
+            }
+            return currPlayer;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab3/Othello/Othello/src/Cecs475.Othello.Application/OthelloScoreConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Cecs475.Othello.Model;
+
+namespace Cecs475.Othello.Application
+{
+    public class OthelloScoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GameAdvantage adv = (GameAdvantage)value;
+            String score = "";
+            if(adv.Player == 1)
+            {
+                score = "Black is winning by " + adv.Advantage;
+            }else if(adv.Player == 2)
+            {
+                score = "White is winning by " + adv.Advantage;
+            }
+            else
+            {
+                score = "Tie Game";
+            }
+            
+            return score;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
